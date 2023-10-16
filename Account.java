14790ed873c8dea433709160d665ecc557309f32
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2023-10-15 20:50:25 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,28 +1,63 @@
+/**
+ Represents an Account with information about the holder 
+ and the amount of money in the account itself
+ @author Pranav Gummaluri, Yasasvi Tallapaneni
+ */
 public abstract class Account implements Comparable<Account> {
     protected Profile holder;
     protected double balance;
 
+    /**
+     * This constructor initializes the holder and balance 
+     * fields when an Account object is created.
+     * @param Profile holder representing the full name and date of birth
+     * @param double balance representing the account balance in dollars
+     */
     public Account(Profile holder, double balance) {
         this.holder = holder;
         this.balance = balance;
     }
 
+    /**
+    * Calculates the monthly interest for the account
+    * Calculates the monthly fee for the account
+    */
     public abstract double monthlyInterest();
     public abstract double monthlyFee();
+    
+    //@return the account type
     public Profile getHolder() {
         return holder;
     }
 
+    //Getter methods
+    /**
+    * Returns the balance of the account
+    * @return a double representing the balance of account
+    */
     public double getBalance() {
         return balance;
     }
 
+    /**
+    * Returns the type of the account
+    * @return a String representing the type of account
+    */
     public abstract String getAccountType();
 
+    /**
+    * Adds the specified amount to the account balance.
+    * @param double amount representing the amount of money the user wants to deposit
+    */
     public void deposit(double amount) {
         balance += amount;
     }
 
+    /**
+    * Returns a boolean representing if the withdrawal was a success or failure
+    * @param double amount representing the amount of money the user wants to withdraw
+    * @return boolean representing if the withdrawal was a success or failure
+    */
     public boolean withdraw(double amount) {
         if (balance >= amount) {
             balance -= amount;
@@ -31,6 +66,11 @@ public abstract class Account implements Comparable<Account> {
         return false;
     }
 
+    /**
+    * A comparison method for sorting accounts. 
+    * @param an account object representing account types and names and dates of bith
+    * @return 1, 0, -1 representing comparison status
+    */
     @Override
     public int compareTo(Account other) {
         int accountTypeComparison = this.getAccountType().compareTo(other.getAccountType());
@@ -54,7 +94,11 @@ public abstract class Account implements Comparable<Account> {
 
 
 
-
+    /*
+    * Compares Account objects based on the holder profiles.
+    * @param Object obj representing account holder and profiles
+    * @return boolean indicating equal or unequal objects
+    **/
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -63,11 +107,19 @@ public abstract class Account implements Comparable<Account> {
         return holder.equals(other.holder);
     }
 
+    /*
+    * Generates a hashcode based on the holder profiles.
+    * @return an int hashcode based on the holder profile
+    **/
     @Override
     public int hashCode() {
         return holder.hashCode();
     }
 
+    /**
+    * Returns a string of all the detailes of the Account object
+    * @return a string representation of the Account object
+    */
     @Override
     public String toString() {
 
@@ -75,6 +127,7 @@ public abstract class Account implements Comparable<Account> {
 
     }
 
+    //Empty method
     public void resetWithdrawals() {
     }
 }
