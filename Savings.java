36b4c4a21dc3e26105a911c9311b7ed43c928d4b
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2023-10-16 12:45:06 -0400

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,18 +1,32 @@
+/*
+* A specific type of account that extends the Account class. 
+* @authors Pranav Gummaluri, Yasasvi Tallapaneni
+*/
+
 public class Savings extends Account {
     private static final double ANNUAL_INTEREST_RATE = 0.04;
     private static final double MONTHLY_FEE = 25.0;
     protected boolean isLoyal;
 
+    /*
+    * Aonstructor that takes a Profile (account holder), an initial balance, and a boolean isLoyal as parameters
+    * @param Profile (account holder), double initial balance, and boolean isLoyal
+    */
     public Savings(Profile holder, double balance, boolean isLoyal) {
         super(holder, balance);
         this.isLoyal = isLoyal;
     }
 
+    //Getter method
     @Override
     public String getAccountType() {
         return "S";
     }
 
+    /*
+    * Calculates the monthly interest based on the balance and loyalty status
+    * @return the balance with a potential 0.25% interest based on the conditions
+    */
     @Override
     public double monthlyInterest() {
 
@@ -23,6 +37,10 @@ public class Savings extends Account {
         return balance * (interestRate / 12);
     }
 
+    /*
+    * Calculates the monthly fee based on the balance.
+    * @return a double indicating a monthly fee based on the conditions
+    */
     @Override
     public double monthlyFee() {
 
@@ -34,6 +52,7 @@ public class Savings extends Account {
 
     }
 
+    //@return String representation of the savings account
     @Override
     public String toString() {
         if (isLoyal) {
